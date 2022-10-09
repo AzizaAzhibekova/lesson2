ebd61dcb4eb74a2d0c8b9d4d0e44b548cc88502e
aziza <azizazhibekova@gmail.com>
2022-10-10 01:56:56 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java");
+       /* System.out.println("Moskow");   //
+
+        System.out.printf("My name is %s and I am %d "," Aziza" , 18);*/
+        //%s бутун сан строка символов
+        //%d целое десятичное число
+        //%f вещественное чисто хх.ххх
+        //\%C символ
+        // System.out.printf("Java Moskow %c",'*');
+        //111   System.out.printf("%f This is %s a string,%d ",10.4,"hihi",10000)
+//byte,short,int,long бутун сандар учун
+        //
+
+//        byte san = -128;
+//        byte san1=27;
+//         System.out.println("Menin jashym "+san1);
+//         System.out.println("Menin jashym "+27);
+//         short shortSan=23344;
+//          short mInSan=-32768;
+//          System.out.println(shortSan-mInSan);
+//          System.out.println(mInSan+3);
+//          int intSan=1237888990;
+//          int intSan2=72618736;
+//          long longSan=923712984 679798l;
+//
+//
+//             int IntSan=54;
+//             int minSan2=+45;
+//           System.out.println(IntSan-minSan2);
+//           System.out.println(mInSan+minSan2);
+
+//       Scanner sc=new Scanner(System.in) ;
+//        int san = 2;
+//        int san2 = sc. nextInt();
+//         System.out.println((san2+san)/2d
+//         );
+        /**
+         *
+         *
+         *
+         *
+         */
+
+//              Scanner sc = new Scanner(System.in);
+//              short a =  sc.nextShort();
+//              short b = sc.nextShort();
+//            System.out.println( a + b);
+//            System.out.println(a / b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+
+
+//          Scanner sc=new Scanner(System.in);
+//         long a = 2022;
+//         long b = sc.nextInt();
+//           System.out.println(a-b);
+
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Menin Jashym " + a);
+        System.out.println("Menin Jashym Emes " + b);
+
+
+    }
+}
